9217263ecf56473d23befe06655f5aabc7874a57
Joonyoung <blackhound97@gmail.com>
2024-05-08 13:36:56 +0900

    routes controller, sequelize 등 기본 설정

--- main/app.js
+++ main/app.js
@@ -1,17 +1,29 @@
 const express =require('express');
 const app = express();
 const PORT = 8000;
+const router = require('./routes')
+const { sequelize } = require('./models');
 
 app.set('view engine','ejs');
 app.set('views','./views');
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+
 app.use('/static',express.static(__dirname + '/static'))
 
-const indexRouter = require('./routes/index')
-app.use('/', indexRouter);
+app.use('/', router);
 
-app.listen(PORT,()=>{
-  console.log(`${PORT}번 포트에서 서버 실행중 . . . `);
-})
+// const userRouter = require('./routes/user')
+// app.use('/user',userRouter);
+
+sequelize
+  .sync({force:false})
+  .then(()=>{
+    app.listen(PORT,()=>{
+      console.log(`${PORT}번 포트에서 서버 실행중 . . . `);
+    })
+  })
+  .catch((err)=>{
+    console.error(err);
+  })
