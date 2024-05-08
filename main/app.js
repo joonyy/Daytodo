659664ccfd24bbcfacb53a6fca65e858dd849872
Joonyoung <blackhound97@gmail.com>
2024-05-08 11:56:53 +0900

    기본 항목 다운로드

---
+++ main/app.js
@@ -0,0 +1,14 @@
+const express =require('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use('/static',express.static(__dirname + '/static'))
+
+app.listen(PORT,()=>{
+  console.log(`${PORT}번 포트에서 서버 실행중 . . . `);
+})
