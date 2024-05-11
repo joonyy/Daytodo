aeecec63efbea250d73ab58b92aacee4212e3c10
dongmyeong930213 <dongmyeong930213@gmail.com>
2024-05-11 12:55:55 +0900

    project DayTodo

---
+++ main/static/js/dailyView.js
@@ -0,0 +1,21 @@
+function showTodo(){
+    // 여기는 투두항목을 자세히 보기 위한 axios : get 요청을 보내려고 해요
+    console.log('Todo를 자세히 보여줌');
+  }
+  
+  function editTodo(){
+    // todo 수정하기, axios : get요청을 통해 input들에 data를 뿌려줄거에요
+    // get요청 시 받아온 date를 보내줘야 해요
+    console.log('Todo 수정하기');
+  }
+  
+  function updateTodo(){
+    // Todo 항목을 수정할 수 있는 axios : patch 요청이에요. 
+    console.log('TODO를 업데이트');
+  }
+  
+  function showCreateTodoText(){
+    // +버튼에 hover 시 Todo 작성하기 텍스트 보여주기
+    console.log('todo를 만들어볼까요~');
+  }
+  
