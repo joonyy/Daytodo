ce7ba762cb8f8132bae0e70cb7b2f8120b74a81d
Joonyoung <blackhound97@gmail.com>
2024-05-11 10:34:14 +0900

    router, controller, dailyView 함수 연결작업 + 이름변경

--- main/routes/todo.js
+++ main/routes/todo.js
@@ -2,11 +2,10 @@ const express =require('express');
 const router = express.Router();
 const controller = require('../controller/Todo')
 
-router.get('/month',controller.month);
-// router.get('/week',controller);
-// router.get('/date',controller);
-// router.get('/date/:date',controller);
-// router.get('/date/:id',controller);
+router.get('/',controller.month);
+router.get('/week',controller.week);
+router.get('/date',controller.date);
+router.get('/date/:{date}', controller.showTodo);
 
 
 module.exports = router;
