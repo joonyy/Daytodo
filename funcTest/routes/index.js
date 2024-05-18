c803f6df0f6eff8b3aa10ec58c7fbe25e016d5ea
Joonyoung <blackhound97@gmail.com>
2024-05-18 13:35:09 +0900

    임시저장

--- funcTest/routes/index.js
+++ funcTest/routes/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controller/Ctodo');
 
+//메인 : 기본 접속 시, 달력을 렌더링한다.
 router.get('/', controller.main);
+
+//week : date => 클릭 시, controller
 router.get('/week',controller.week);
 router.get('/date',controller.date);
 router.get('/getThisMonthTodos', controller.thisMonth);
