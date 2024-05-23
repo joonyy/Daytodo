e40dafcd2a7e00807e7f7cd4e7b6b41be6b836c0
Joonyoung <blackhound97@gmail.com>
2024-05-24 00:06:50 +0900

    파일 병합

--- main/app.js
+++ main/app.js
@@ -9,18 +9,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/static', express.static(__dirname + '/static'));
 
-const indexRouter = require('./routes/index');
-app.use('/', indexRouter);
+const todoRouter = require('./routes/todoRouter');
+app.use('/', todoRouter);
 
-app.get('/date', (req, res) => {
-    res.render('date');
-  });  // Createtodo 경로 종료
-
-  app.get('/viewtodo', (req, res) => {
-    res.render('viewtodo');
-  }); //viewtodo 경로 종료
-
-app.get('/week')
+const loginRouter = require('./routes/loginRouter');
+app.use('/login', loginRouter);
 
 app.listen(PORT, () => {
     console.log(`${PORT}번 포트에서 서버 실행중 . . . `);
